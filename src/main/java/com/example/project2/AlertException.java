4d548d5914eb3ed8fd7934ee51d0010737b36367
WLF225 <hima200310@gmail.com>
2025-03-27 16:50:34 +0200

    project2 0.1

--- src/main/java/com/example/project2/AlertException.java
+++ src/main/java/com/example/project2/AlertException.java
@@ -5,10 +5,5 @@ import javafx.scene.control.Alert;
 public class AlertException extends RuntimeException {
     public AlertException(String message) {
         super(message);
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Error");
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        alert.showAndWait();
     }
 }
